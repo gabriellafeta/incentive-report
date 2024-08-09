101166f58175d0eccef7756f00236680e8e45c48
Gabriel Lafetá <135139722+gabriellafeta@users.noreply.github.com>
2024-08-09 05:51:08 +0000

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,18 @@
+# importando arquivos
+import pandas as pd
 import streamlit as st
+from azure.storage.blob import BlobServiceClient, BlobClient
+from azure.core.exceptions import ResourceExistsError
+from io import StringIO
+import os
+import plotly.express as px
+import plotly.graph_objects as go
+import numpy as np
+from datetime import datetime
+
+#------------------------------------------------------------------------------------------------------
+st.set_page_config(layout="wide") # Configuração da página larga
+#------------------------------------------------------------------------------------------------------
 
 st.title("🎈 My new app")
 st.write(
