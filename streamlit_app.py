67a59978e2bc2b8557fa691d412d310186091e45
Gabriel Lafetá <135139722+gabriellafeta@users.noreply.github.com>
2024-08-09 06:15:54 +0000

    commitcommit

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,39 @@ from datetime import datetime
 st.set_page_config(layout="wide") # Configuração da página larga
 #------------------------------------------------------------------------------------------------------
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start bcu eamlit.io](https://docs.streamlit.io/)."
-)
+# Uploading data
+
+connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+
+if connection_string is None:
+    raise Exception("Environment variable AZURE_STORAGE_CONNECTION_STRING is not set.")
+
+blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+
+container_name = 'expansionbees0001'
+local_file_path = r'C:\Users\gabri\OneDrive\Área de Trabalho\DataID'
+blob_name = 'blob0001'
+
+container_client = blob_service_client.get_container_client(container_name)
+
+#------------------------------------------------------------------------------------------------------
+
+#### Mandar arquivos na pasta DataID para o Azure Blob Storage
+##### Tables from Blob
+                
+blob_name = 'sales_incentive.csv'
+blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+blob_content = blob_client.download_blob().content_as_text()
+sales_incentive = StringIO(blob_content)
+sales_incentive_df = pd.read_csv(sales_incentive)
+
+
+st.title('Sales Incentive Data')
+
+# Display the DataFrame
+st.dataframe(sales_incentive_df)
+
+
+
+
+
